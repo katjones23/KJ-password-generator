6b1b8dc96b4e45044e0818e2e41cae5e7dc76e4e
Katherine Jones <kejone1414@gmail.com>
2020-03-16 17:56:52 -0400

    added validation for confirms

--- docs/script.js
+++ docs/script.js
@@ -14,13 +14,22 @@ function generatePassword() {
   // var numericExpression = /^[0-9]+$/;
   var passLength = parseInt(prompt("How long would you like your password to be?\nPlease enter a value between 8 and 128", "8"));
   var confirmLowercase = confirm("Do you want to use lowercase characters?")
-  var confirmUppercase = confirm("Do you want to use uppercase characters?")
-  var confirmNumeric = confirm("Do you want to use numbers?")
-  var confirmSpecial = confirm("Do you want to use special characters?")
+  var confirmUppercase = confirm("Do you want to use uppercase characters?");
+  var confirmNumeric = confirm("Do you want to use numbers?");
+  var confirmSpecial = confirm("Do you want to use special characters?");
 
+  var confirmArray = [confirmLowercase, confirmUppercase, confirmNumeric, confirmSpecial];
+  var checkFalse = confirmArray.every(confirmValidation);
 
+  function confirmValidation (value) {
+    return value === false;
+  }
 
-}
+  if (checkFalse) {
+    alert("Please select at least one character type.")
+  };
+
+};
 
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
